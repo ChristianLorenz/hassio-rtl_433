2fedd133b20f298109a597ddc292c7a510b948e7
Chris Lorenz <Christian.Lorenz@gmail.com>
2020-08-24 09:32:46 -0700

    Update run.sh

--- rtl_433/run.sh
+++ rtl_433/run.sh
@@ -4,4 +4,6 @@ MQTT_HOST=$(bashio::services mqtt "host")
 MQTT_USER=$(bashio::services mqtt "username")
 MQTT_PASSWORD=$(bashio::services mqtt "password")
 
+echo mqtt://$MQTT_HOST,user=$MQTT_USER
+
 /usr/local/bin/rtl_433 -c /config/rtl_433.conf -F mqtt://$MQTT_HOST,user=$MQTT_USER,pass=$MQTT_PASSWORD,retain=0,devices=rtl_433/[model:-]_[id:-][channel]
